c9db37b61bc6dd588fc929a148076e3e1c83a5be
kyleslater <37429591+kyleslater@users.noreply.github.com>
2020-09-18 01:03:07 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,125 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
+
+function showDetails() {
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+function setDetails(imageUrl, titleText) {
+    'use strict';
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict';
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+        showDetails();
+    });
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+}
+
+function initializeEvents() {
+    'use strict';
+    var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+}
+
+function getDetailImg() {
+    var imgDir = document.getElementsByClassName("detail-image")[0].getAttribute("src");
+    return imgDir;
+}
+
+function nextImage() {
+    switch (getDetailImg()) {
+        case "img/otter1.jpg":
+            setDetails("img/otter2.jpg", "How Deep Is Your Love");
+            showDetails();
+            break;
+        case "img/otter2.jpg":
+            setDetails("img/otter3.jpg", "You Should be Dancing");
+            showDetails();
+            break;
+        case "img/otter3.jpg":
+            setDetails("img/otter4.jpg", "Night Fever");
+            showDetails();
+            break;
+        case "img/otter4.jpg":
+            setDetails("img/otter5.jpg", "To Love Somebody");
+            showDetails();
+            break;
+        case "img/otter5.jpg":
+            setDetails("img/otter1.jpg", "Stayin' Alive");
+            showDetails();
+            break;
+        default:
+            break;
+    }
+}
+
+function previousImage() {
+    switch (getDetailImg()) {
+        case "img/otter1.jpg":
+            setDetails("img/otter5.jpg", "To Love Somebody");
+            showDetails();
+            break;
+        case "img/otter2.jpg":
+            setDetails("img/otter1.jpg", "Stayin' Alive");
+            showDetails();
+            break;
+        case "img/otter3.jpg":
+            setDetails("img/otter2.jpg", "How Deep Is Your Love");
+            showDetails();
+            break;
+        case "img/otter4.jpg":
+            setDetails("img/otter3.jpg", "You Should be Dancing");
+            showDetails();
+            break;
+        case "img/otter5.jpg":
+            setDetails("img/otter4.jpg", "Night Fever");
+            showDetails();
+            break;
+        default:
+            break;
+    }
+}
+
+initializeEvents();
